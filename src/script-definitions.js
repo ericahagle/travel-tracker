e216096a1e0982c1418e0d8a7a6311e59a51d905
Erica Hagle <ericahagle.dev@gmail.com>
2023-11-10 23:14:16 -0800

    feat: Add function to get complete traveler data

--- src/script-definitions.js
+++ src/script-definitions.js
@@ -1,4 +1,17 @@
 ////////////////////* Functions *////////////////////
-
+function getAllTravelerData(traveler, trips, destinations) {
+  const travelerTripsData = trips.filter((trip) => {
+    return trip.userID === traveler.id;
+  });
+  const travelerDestinationsData = destinations.filter((destination) => {
+    return destination.id === trips.destinationID;
+  });
+  const completeTravelerData = {
+    ...traveler,
+    tripsData: travelerTripsData || [],
+    destinationsData: travelerDestinationsData || []
+  };
+  return completeTravelerData;
+}
 
 ////////////////////* Exports *////////////////////
