33082bf4a95456e1cbce46d4b296055707f5ed8a
Erica Hagle <ericahagle.dev@gmail.com>
2023-11-11 12:13:33 -0800

    feat: Setup fetch skeletons and exports

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,7 +1,35 @@
 ////////////////////* Global Variables *////////////////////
-
+const travelerAPI = 'http://localhost:3001/api/v1/travelers/id';
+const allTravelersAPI = 'http://localhost:3001/api/v1/travelers';
+const tripsAPI = 'http://localhost:3001/api/v1/trips';
+const destinationsAPI = 'http://localhost:3001/api/v1/destinations';
 
 ////////////////////* Fetch Calls *////////////////////
+const fetchTraveler = () => {
 
+}
+
+const fetchAllTravelers = () => {
+
+}
+
+const fetchTrips = () => {
+
+}
+
+const fetchDestinations = () => {
+
+}
+
+const fetchAllData = () => {
+
+}
 
 ////////////////////* Exports *////////////////////
+export {
+  fetchTraveler,
+  fetchAllTravelers,
+  fetchTrips,
+  fetchDestinations,
+  fetchAllData
+}
